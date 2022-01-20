eed783427086950e47814fa1b26511db53b95f79
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-01-20 14:29:50 +0530

    filters by useReducer completed

--- lovepen/src/context/ProductContext.js
+++ lovepen/src/context/ProductContext.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState , useReducer } from "react";
 import {productReducer} from "../reducer/productReducer"
+import {filterReducer}  from "../reducer/filterReducer"
 //our createContext or our global state
 export const ProductContext = createContext();
 
@@ -8,8 +9,11 @@ export function ProductProvider({ children }) {
 
   const [state,productDispatch]= useReducer(productReducer , {products:[]})
 
+  const[filterState , filterDispatch] = useReducer(filterReducer ,{searchQuery :''})
+
+
   return (
-    <ProductContext.Provider value={{ state,productDispatch }}>
+    <ProductContext.Provider value={{ state,productDispatch  , filterState , filterDispatch}}>
       {children}
     </ProductContext.Provider>
   );
