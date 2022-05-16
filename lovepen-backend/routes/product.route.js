cbb1067beed46dc2c4cb127ddcc9dc926f460e53
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-05-16 10:06:58 +0530

    Basic wiring SETUP

---
+++ lovepen-backend/routes/product.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { getAllProducts } = require("../controllers/product.controller");
+
+const router = express.Router();
+
+router.route("/products").get(getAllProducts);
+module.exports = router
--- lovepen-backend/server.js
+++ lovepen-backend/server.js
@@ -1,13 +1,24 @@
-const app = require("./app")
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
 
 const dotenv = require("dotenv")
 
 //Config
 
-dotenv.config({path : "lovepen-backend/config.env"})
+dotenv.config({path : "lovepen-backend/config/config.env"})
 
 
+//Route Import 
+const product = require("./routes/product.route")
 
-app.listen(process.env.PORT ,()=>{
+app.use("/api/v1" , product)
+
+
+app.listen(process.env.PORT,()=>{
     console.log(`Server is working on http://localhost: ${process.env.PORT}`)
+    
 })
+
