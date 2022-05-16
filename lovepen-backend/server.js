b545c972c91363c043c1e839b00d9617f6d94732
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-05-16 11:39:08 +0530

    database-setup (MongoDB)

--- lovepen-backend/server.js
+++ lovepen-backend/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
-const port = process.env.PORT || 4000;
+const dotenv  = require("dotenv")
+
+const connectDatabase = require("./database/DBConnect")
 
 const app = express();
 
 app.use(express.json())
+
+
+//Config
+
+dotenv.config({path: "lovepen-backend/config/config.env"})
+
+//Connect to database 
+connectDatabase()
+
 
 
 //Route Import 
@@ -15,8 +26,8 @@ const product = require("./routes/product.route")
 app.use("/api/v1" , product)
 
 
-app.listen(port,()=>{
-    console.log(`Server is working on http://localhost: `,port)
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is working on http://localhost: ${process.env.PORT}`)
     
 })
 
