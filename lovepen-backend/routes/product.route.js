20c05cee62436d2d8794d0e518e0c8d8c50ede24
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-05-18 19:03:00 +0530

    Error Handling

--- lovepen-backend/routes/product.route.js
+++ lovepen-backend/routes/product.route.js
@@ -4,10 +4,16 @@ const { getAllProducts ,createProduct , updateProduct , getProductById, deletePr
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
-router.route("/products/:id").get(getProductById)
-router.route("/products/new").post(createProduct);
-router.route("/products/:id").post(updateProduct);
-router.route("/products/:id").delete(deleteProduct)
+
+router.route('/').get(getAllProducts).post(createProduct);
+router.route('/:id').get(getProductById);
+
+
+//TRIED FOR BACKEND 
+
+// router.route("/products/:id").get(getProductById)
+// router.route("/products/new").post(createProduct);
+// router.route("/products/:id").post(updateProduct);
+// router.route("/products/:id").delete(deleteProduct)
 
 module.exports = router
--- lovepen-backend/server.js
+++ lovepen-backend/server.js
@@ -1,21 +1,33 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const dotenv  = require("dotenv")
-
-const connectDatabase = require("./database/DBConnect")
-
-const app = express();
-
-app.use(express.json())
-
 
 //Config
 
 dotenv.config({path: "lovepen-backend/config/config.env"})
 
+const cors = require('cors');
+
+const routeNotFoundHandler = require('./middlewares/route-not-found.middleware');
+const allErrorsHandler = require('./middlewares/all-error-handler.middleware');
+
+const connectDatabase = require("./database/DBConnect")
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use(cors());
+
+
 //Connect to database 
 connectDatabase()
 
+app.get('/', (req, res) => {
+	res.send('Welcome to the LovePen store');
+});
 
 
 //Route Import 
@@ -24,6 +36,19 @@ const product = require("./routes/product.route")
 //Use the route 
 
 app.use("/api/v1" , product)
+
+
+/**
+ * 404 Route Handler
+ * Note: Do Not Move. This should be the last route.
+ */
+ app.use(routeNotFoundHandler);
+
+ /**
+  * Error Handler
+  * Note: Do Not Move.
+  */
+ app.use(allErrorsHandler);
 
 
 app.listen(process.env.PORT,()=>{
